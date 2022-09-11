95f9c30acf488619b08134e3c05330e5f5458d64
Mariusz Kogen <k0gen@pm.me>
2022-09-11 15:57:42 +0200

    Added internal drive support

--- speed-test.sh
+++ speed-test.sh
@@ -3,7 +3,13 @@ echo
 echo " embassyOS Disk Speed Test"
 echo
 echo "Found:"
+detect=$(sudo lshw -class disk -class volume | grep -Ev '*-disk|description|physical|bus|logical|version|serial|size|capabilities' 2>&1)
+if [ -z "$detect" ]
+then
+sudo lshw -class disk -class storage | grep -Ev '*-storage|description|physical|bus|logical|version|serial|size|capabilities|width|clock|configuration|resources'
+else
 sudo lshw -class disk -class volume | grep -Ev '*-disk|description|physical|bus|logical|version|serial|size|capabilities'
+fi
 echo "testing ..."
 twrite=$(sudo dd if=/dev/zero of=/embassy-data/main/stest oflag=direct bs=128k count=16k 2>&1)
 twrite=$(echo ${twrite##*s,})
