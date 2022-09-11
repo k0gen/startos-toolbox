9456e2cf5b3e77757f1c9a7a34a29321a3fc9130
Mariusz Kogen <k0gen@pm.me>
2022-09-11 13:21:16 +0200

    Create speed-test.sh

---
+++ speed-test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo
+echo " embassyOS Disk Speed Test"
+echo
+echo " testing ..."
+twrite=$(sudo dd if=/dev/zero of=/embassy-data/main/stest oflag=direct bs=128k count=16k 2>&1)
+twrite=$(echo ${twrite##*s,})
+echo " WRITE speed test = $twrite"
+tread=$(dd if=/embassy-data/main/stest of=/dev/null bs=128k count=16k 2>&1)
+tread=$(echo ${tread##*s,})
+echo " READ speed test = $tread"
+sudo rm -f /embassy-data/main/stest
+echo
+echo "Done!"
