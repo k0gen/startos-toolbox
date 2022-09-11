999dc2a3e62378395380573ab80f192b7db75a56
Mariusz Kogen <k0gen@pm.me>
2022-09-11 15:27:41 +0200

    Added hardware detection

--- speed-test.sh
+++ speed-test.sh
@@ -2,7 +2,9 @@
 echo
 echo " embassyOS Disk Speed Test"
 echo
-echo " testing ..."
+echo "Found:"
+sudo lshw -class disk -class volume | grep -Ev '*-disk|description|physical|bus|logical|version|serial|size|capabilities'
+echo "testing ..."
 twrite=$(sudo dd if=/dev/zero of=/embassy-data/main/stest oflag=direct bs=128k count=16k 2>&1)
 twrite=$(echo ${twrite##*s,})
 echo " WRITE speed test = $twrite"
