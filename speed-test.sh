3c05108d78cb06d218ec18322fadce0597d1bff7
Mariusz Kogen <k0gen@pm.me>
2022-09-12 18:37:02 +0200

    added fdatasync to include flush and sync time

--- speed-test.sh
+++ speed-test.sh
@@ -9,22 +9,21 @@ echo "Found: $(lsblk -d -o TRAN,MODEL,SIZE | sed -n '5p')"
 else
 echo "Found: $(lsblk -S -o TRAN,VENDOR,MODEL,SIZE | sed -n '2p')"
 fi
-sudo sh -c "sync && echo 3 > /proc/sys/vm/drop_caches"
 echo
 echo "testing ..."
-twrite=$(sudo dd if=/dev/zero of=/embassy-data/main/stest oflag=direct bs=128k count=8k 2>&1)
+twrite=$(sudo dd if=/dev/zero of=/embassy-data/main/stest conv=fdatasync oflag=direct bs=128k count=8k 2>&1)
 twrite=$(echo ${twrite##*s,})
-echo " WRITE speed test = $twrite (direct)"
-tswrite=$(sudo dd if=/dev/zero of=/embassy-data/main/stest bs=128k count=8k 2>&1)
+echo " WRITE speed test (direct) = $twrite"
+tswrite=$(sudo dd if=/dev/zero of=/embassy-data/main/stest conv=fdatasync bs=128k count=8k 2>&1)
 tswrite=$(echo ${tswrite##*s,})
-echo " WRITE speed test = $tswrite (cached)"
+echo " WRITE speed test (cached) = $tswrite"
 sudo sh -c "sync && echo 3 > /proc/sys/vm/drop_caches"
-tread=$(dd if=/embassy-data/main/stest of=/dev/null bs=128k count=8k 2>&1)
+tread=$(dd if=/embassy-data/main/stest of=/dev/null conv=fdatasync bs=128k count=8k 2>&1)
 tread=$(echo ${tread##*s,})
-echo "  READ speed test = $tread (direct)"
-tcread=$(dd if=/embassy-data/main/stest of=/dev/null bs=128k count=1k 2>&1)
+echo "  READ speed test (direct) = $tread"
+tcread=$(dd if=/embassy-data/main/stest of=/dev/null conv=fdatasync bs=128k count=8k 2>&1)
 tcread=$(echo ${tcread##*s,})
-echo "  READ speed test = $tcread (cached - crazy)"
+echo "  READ speed test (cached) = $tcread (crazy)"
 sudo rm -f /embassy-data/main/stest
 echo
 echo "Done!"
