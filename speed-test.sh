0c4278dca976b78a6c050d8f481b380e734659f3
Mariusz Kogen <k0gen@pm.me>
2022-09-13 10:25:47 +0200

    Update speed-test.sh

--- speed-test.sh
+++ speed-test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Define directory on target drive here
+target="/embassy-data/main"
 echo
 echo " embassyOS Disk Speed Test"
 echo
@@ -11,19 +13,19 @@ echo "Found: $(lsblk -S -o TRAN,VENDOR,MODEL,SIZE | sed -n '2p')"
 fi
 echo
 echo "testing ..."
-twrite=$(sudo dd if=/dev/zero of=/embassy-data/main/stest conv=fdatasync oflag=direct bs=128k count=8k 2>&1)
+twrite=$(sudo dd if=/dev/zero of=$target/stest conv=fdatasync oflag=direct bs=128k count=8k 2>&1)
 twrite=$(echo ${twrite##*s,})
 echo " WRITE speed test (direct) = $twrite"
-tswrite=$(sudo dd if=/dev/zero of=/embassy-data/main/stest conv=fdatasync bs=128k count=8k 2>&1)
+tswrite=$(sudo dd if=/dev/zero of=$target/stest conv=fdatasync bs=128k count=8k 2>&1)
 tswrite=$(echo ${tswrite##*s,})
 echo " WRITE speed test (cached) = $tswrite"
 sudo sh -c "sync && echo 3 > /proc/sys/vm/drop_caches"
-tread=$(dd if=/embassy-data/main/stest of=/dev/null conv=fdatasync bs=128k count=8k 2>&1)
+tread=$(dd if=$target/stest of=/dev/null conv=fdatasync bs=128k count=8k 2>&1)
 tread=$(echo ${tread##*s,})
 echo "  READ speed test (direct) = $tread"
-tcread=$(dd if=/embassy-data/main/stest of=/dev/null conv=fdatasync bs=128k count=8k 2>&1)
+tcread=$(dd if=$target/stest of=/dev/null conv=fdatasync bs=128k count=8k 2>&1)
 tcread=$(echo ${tcread##*s,})
 echo "  READ speed test (cached) = $tcread (crazy)"
-sudo rm -f /embassy-data/main/stest
+sudo rm -f $target/stest
 echo
 echo "Done!"
