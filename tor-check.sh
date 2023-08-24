7c8972821a4138e0dcc00d54aa136131f2b845b7
Mariusz Kogen <k0gen@pm.me>
2023-08-24 13:54:04 +0200

    Implement onion_list array
    In this version, each element in the `onion_list` array is formatted as "Name|URL". The loop then extracts the name and URL by splitting each element at the delimiter "|".

--- tor-check.sh
+++ tor-check.sh
@@ -1,36 +1,30 @@
 #!/bin/sh
-fail=$(printf " [\e[31m fail \e[0m]")
-pass=$(printf " [\e[32m pass \e[0m]")
+
+fail=$(printf " [\033[31m fail \033[0m]")
+pass=$(printf " [\033[32m pass \033[0m]")
+
+onion_list=(
+    "Start9|http://privacy34kn4ez3y3nijweec6w4g54i3g54sdv7r5mr6soma3w4begyd.onion"
+    "Mempool|http://mempoolhqx4isw62xs7abwphsq7ldayuidyx2v2oethdhhj6mlo2r6ad.onion"
+    "DuckDuckGo|https://duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion"
+    "Brave Search|https://search.brave4u7jddbv7cyviptqjc7jusxh72uik7zt6adtckl5f4nwy2v72qd.onion"
+)
 
 echo "Testing connection to Onion Pages ..."
-curl --socks5-hostname localhost:9050 http://privacy34kn4ez3y3nijweec6w4g54i3g54sdv7r5mr6soma3w4begyd.onion/latest > /dev/null 2>&1
-HC=$?
-if [ $HC -ne 0 ]
-then
-        echo " ${fail}: Start9 Page not reachable."
-elif [ $HC -eq 0 ]
-then
-        echo " ${pass}: Start9 Page reachable"
-fi
 
-curl --socks5-hostname localhost:9050 http://mempoolhqx4isw62xs7abwphsq7ldayuidyx2v2oethdhhj6mlo2r6ad.onion > /dev/null 2>&1
-HC=$?
-if [ $HC -ne 0 ]
-then
-        echo " ${fail}: Mampool Page not reachable."
-elif [ $HC -eq 0 ]
-then
-        echo " ${pass}: Mempool Page reachable"
-fi
+for data in "${onion_list[@]}"; do
+    name="${data%%|*}"
+    url="${data#*|}"
+    
+    curl --socks5-hostname localhost:9050 "$url" > /dev/null 2>&1
+    HC=$?
+    
+    if [ $HC -ne 0 ]; then
+        echo " ${fail}: $name ($url) "
+    else
+        echo " ${pass}: $name ($url) "
+    fi
+done
 
-curl --socks5-hostname localhost:9050 https://duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion/ > /dev/null 2>&1
-HC=$?
-if [ $HC -ne 0 ]
-then
-        echo " ${fail}: DuckDuckGo Page not reachable."
-elif [ $HC -eq 0 ]
-then
-        echo " ${pass}: DuckDuckGo Page reachable"
-fi
 echo
 echo "Done."
