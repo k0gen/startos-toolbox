17b34c0a77c4f5a7a6d4a456dae6214f6848c334
Mariusz Kogen <k0gen@pm.me>
2025-02-12 07:55:43 +0100

    Update UTXOracle.sh

--- UTXOracle.sh
+++ UTXOracle.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+# Create the bitcoin-cli command
 printf '#!/bin/sh\nexec sudo podman exec -it bitcoind.embassy bitcoin-cli "$@"' | sudo tee /usr/local/bin/bitcoin-cli >/dev/null
 sudo chmod +x /usr/local/bin/bitcoin-cli
+
+# Run the Python script, and it will wait for user input
 python3 <(curl -sL https://utxo.live/oracle/UTXOracle.py)
